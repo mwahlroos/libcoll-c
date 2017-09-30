bc07d0698c5eb8943ff588d36353f3730bef90c1
Mika Wahlroos <mika.wahlroos@iki.fi>
2017-09-30 18:30:41 +0300

    implement retrieval and removal from the hash table

--- hashtable.c
+++ hashtable.c
@@ -67,14 +67,11 @@ void ht_deinit(hashtable_t *ht)
 
 int ht_add(hashtable_t *ht, void *key, void *value)
 {   
-    size_t slot_index;
-    
-    linkedlist_t *collision_list;
     ht_key_value_pair_t *pair;
     
     unsigned long key_hash = hash(ht->hash_value_function(key));
-    slot_index = (key_hash % ht->capacity);
-    collision_list = ht->hash_slots[slot_index];
+    size_t slot_index = (key_hash % ht->capacity);
+    linkedlist_t *collision_list = ht->hash_slots[slot_index];
 
     if (NULL == collision_list) {
         collision_list = ht->hash_slots[slot_index] = ll_init(ht->key_comparator_function);
@@ -90,14 +87,54 @@ int ht_add(hashtable_t *ht, void *key, void *value)
 
 void* ht_get(hashtable_t *ht, void *key)
 {
-    /* FIXME: stub */
-    return NULL;
+    ht_key_value_pair_t *kv_pair;
+    void *retval = NULL;
+
+    unsigned long key_hash = hash(ht->hash_value_function(key));
+    size_t slot_index = (key_hash % ht->capacity);
+    linkedlist_t *collision_list = ht->hash_slots[slot_index];
+
+    if (NULL != collision_list) {
+        ll_iter_t *iter = ll_get_iter(collision_list);
+        while (ll_has_next(iter)) {
+            ll_node_t *node = ll_next(iter);
+            kv_pair = (ht_key_value_pair_t*) (node->value);
+            if (ht->key_comparator_function(key, kv_pair->key) == 0) {
+                retval = kv_pair->value;
+                break;
+            }
+        }
+    }
+
+    return retval;
 }
 
 void* ht_remove(hashtable_t *ht, void *key)
 {
-    /* FIXME: stub */
-    return NULL;
+    ht_key_value_pair_t *kv_pair;
+    void *retval = NULL;
+
+    unsigned long key_hash = hash(ht->hash_value_function(key));
+    size_t slot_index = (key_hash % ht->capacity);
+    linkedlist_t *collision_list = ht->hash_slots[slot_index];
+
+    if (NULL != collision_list) {
+        ll_iter_t *iter = ll_get_iter(collision_list);
+        while (ll_has_next(iter)) {
+            ll_node_t *node = ll_next(iter);
+            kv_pair = (ht_key_value_pair_t*) (node->value);
+            if (ht->key_comparator_function(key, kv_pair->key) == 0) {
+                retval = kv_pair->value;
+                ll_remove_last_returned(iter);
+            }
+        }
+    }
+
+    /* FIXME: should perhaps indicate non-existence of the key somehow
+     * rather than just returning NULL?
+     */
+
+    return retval;
 }
 
 size_t ht_get_capacity(hashtable_t *ht)

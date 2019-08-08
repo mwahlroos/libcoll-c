287507a37d85339e589cadac8991d316284d093e
Mika Wahlroos <mika.wahlroos@iki.fi>
2019-08-09 01:48:23 +0300

    Add types.h that was previously omitted

---
+++ types.h
@@ -0,0 +1,15 @@
+/*
+ * types.h
+ * 
+ * Common types shared between different collection types.
+ */
+
+#ifndef CCOLL_TYPES_H
+#define CCOLL_TYPES_H
+
+typedef struct ccoll_pair_voidptr {
+    void *a;
+    void *b;
+} ccoll_pair_voidptr_t;
+
+#endif /* CCOLL_TYPES_H */
